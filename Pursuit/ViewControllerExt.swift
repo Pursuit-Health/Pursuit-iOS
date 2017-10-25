2b6d461a363408bc30f620d9df3d80143ce79af6
Igor <igor1994makara@gmail.com>
2017-10-25 18:23:45 +0300

    remove all unnecessary files and code

--- Pursuit/ViewControllerExt.swift
+++ Pursuit/ViewControllerExt.swift
@@ -9,8 +9,6 @@
 import Foundation
 import UIKit
 
-//TODO: separate extensions int odifferent file
-
 extension UIViewController {
     func presentDialog(title : String, errorString : String ){
         let alert = UIAlertController(title: title, message: errorString, preferredStyle: .alert)
@@ -18,61 +16,6 @@ extension UIViewController {
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
-    
-    func addReveal(){
-        let revealButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 15))
-        revealButton.setImage(UIImage(named: "hamburgerIcon"), for: .normal)
-        revealButton.addTarget(self, action: #selector(UIViewController.showMenu), for: .touchUpInside)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: revealButton)
-    }
-    
-    func addBack(){
-        let revealButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 18))
-        revealButton.setImage(UIImage(named: "back"), for: .normal)
-        revealButton.addTarget(self, action: #selector(UIViewController.backPressed), for: .touchUpInside)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: revealButton)
-    }
-    
-    func backPressed(){
-        navigationController?.popViewController(animated: true)
-    }
-    
-    func showMenu(){
-        //        self.sideMenuViewController.presentLeftMenuViewController()
-    }
-    
-    func setupKeyboardListeners(){
-        NotificationCenter.default.addObserver(self, selector: #selector(UIViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(UIViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-    }
-    
-    // MARK: - NSNotification
-    func keyboardWillShow(notification: NSNotification) {
-        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! TimeInterval
-        let frame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        
-        UIView.animate(withDuration: duration, animations: { () -> Void in
-            self.setKeyboardWillShowConstraint(height: frame.height)
-        }) { (finished: Bool) -> Void in
-            
-        }
-    }
-    
-    func keyboardWillHide(notification : NSNotification) {
-        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! TimeInterval
-        UIView.animate(withDuration: duration, animations: { () -> Void in
-            self.setKeyboardWillHideConstraint()
-        })
-    }
-    
-    func setKeyboardWillHideConstraint(){
-        
-    }
-    
-    func setKeyboardWillShowConstraint(height : CGFloat){
-        
-    }
-    
 }
 
 import ObjectiveC
@@ -92,24 +35,6 @@ extension UIViewController {
     }
 }
 
-extension UIViewController {
-    func makeTextFieldsFirstResponder(_ textFieldsArray: [AnimatedTextField],_ textField: UITextField) {
-        for (index, value) in textFieldsArray.enumerated() {
-            if value == textField {
-                if index == textFieldsArray.endIndex - 1 {
-                    textFieldsArray[index].resignFirstResponder()
-                    return
-                }
-                textFieldsArray[index + 1].becomeFirstResponder()
-            }
-        }
-    }
-    
-    func animateConstraint(_ textFieldsArray: [DezappTextField],_ textField: UITextField) {
-        
-        
-    }
-}
 
 import ObjectiveC
 extension UIViewController {
